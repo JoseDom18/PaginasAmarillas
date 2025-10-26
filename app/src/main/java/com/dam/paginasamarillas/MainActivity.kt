f7e0d008c343ed9b6c7976d4f9c41f285fef3559
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-26 14:18:17 -0600

    iniciando rama Domingo

--- app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
+++ app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dam.paginasamarillas.ui.theme.PaginasAmarillasTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PaginasAmarillasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            miPrimerTexto();
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun miPrimerTexto(){
+    Column (modifier = Modifier.fillMaxSize().padding(top = 32.dp)){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaginasAmarillasTheme {
-        Greeting("Android")
+        Text(text = "Paginas Amarillas");
+        Text(text = "Bienvenido");
     }
 }
