3a2d0849cf3dec80c39b8cb29a75e724ba3d5bab
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-26 14:21:34 -0600

    inicio de proyecto dam con jetpack compose

--- app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
+++ app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PaginasAmarillasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Páginas Amarillas",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PaginasAmarillasTheme {
-        Greeting("Android")
+        Greeting("Páginas Amarillas")
     }
 }
