30f34c1d4c6dc6b831872e51f516f61d0a8304d2
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-27 00:05:48 -0600

    Avanzando con el curso cajas, filas y columnas, spacers, clases internas, data clases, clases anidadas

--- app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
+++ app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dam.paginasamarillas.ui.theme.PaginasAmarillasTheme
@@ -20,7 +33,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            miPrimerTexto();
+//            miPrimerTexto();
+            cajaPosicion();
         }
     }
 }
@@ -28,8 +42,72 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun miPrimerTexto(){
     Column (modifier = Modifier.fillMaxSize().padding(top = 32.dp)){
+        Row {
+            Text("")
+            Text(text = "Esto es una fila1")
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text = "Esto es una columna")
+                Text(text = "Esto es una Columna2")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = "Esto es una fila2")
+
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
 
         Text(text = "Paginas Amarillas");
         Text(text = "Bienvenido");
     }
+}
+
+@Composable
+fun cajaPosicion(){
+
+    Box(
+        modifier = Modifier.fillMaxSize().padding(32.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .size(50.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.elpunto),
+                contentDescription = "Esta es mi tienda el punto en estilo pixel art",
+                modifier = Modifier
+                    .align(Alignment.CenterVertically).fillMaxSize()
+                    .size(20.dp)
+                    .clip(CircleShape)
+            )
+
+            Column (
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxSize()
+                    .align(Alignment.Top)
+            ) {
+                Text(
+                    text = "Tienda el punto",
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .fillMaxSize()
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun miPrimerPreview(){
+    miPrimerTexto();
+}
+
+@Preview(showBackground = true)
+@Composable
+fun previewBoxImagen(){
+    cajaPosicion();
 }
