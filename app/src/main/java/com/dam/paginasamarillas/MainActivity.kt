0df3f10061bd65828a54019284c9c1020088e62f
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-27 16:20:35 -0600

    empezando login

--- app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
+++ app/src/main/java/com/dam/paginasamarillas/MainActivity.kt
@@ -16,14 +16,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dam.paginasamarillas.ui.theme.PaginasAmarillasTheme
@@ -34,14 +43,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 //            miPrimerTexto();
-            cajaPosicion();
+//            cajaPosicion();
+            login();
         }
+
     }
 }
 
 @Composable
 fun miPrimerTexto(){
-    Column (modifier = Modifier.fillMaxSize().padding(top = 32.dp)){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(top = 32.dp)){
         Row {
             Text("")
             Text(text = "Esto es una fila1")
@@ -66,7 +79,9 @@ fun miPrimerTexto(){
 fun cajaPosicion(){
 
     Box(
-        modifier = Modifier.fillMaxSize().padding(32.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
     ){
         Row(
             modifier = Modifier
@@ -77,7 +92,8 @@ fun cajaPosicion(){
                 painter = painterResource(id = R.drawable.elpunto),
                 contentDescription = "Esta es mi tienda el punto en estilo pixel art",
                 modifier = Modifier
-                    .align(Alignment.CenterVertically).fillMaxSize()
+                    .align(Alignment.CenterVertically)
+                    .fillMaxSize()
                     .size(20.dp)
                     .clip(CircleShape)
             )
@@ -110,4 +126,61 @@ fun miPrimerPreview(){
 @Composable
 fun previewBoxImagen(){
     cajaPosicion();
+}
+
+@Composable
+fun login(){
+    var email : String by remember { mutableStateOf("") };
+    var password : String by remember { mutableStateOf("") };
+    Box(
+        modifier = Modifier
+            .padding(all = 50.dp)
+    ){
+            Text(
+                text = stringResource(R.string.bienvenido_a_p_ginas_amarillas),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+                    .align(Alignment.TopCenter)
+
+
+            )
+        Column {
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it},
+                label = {Text(stringResource(R.string.correo_electronico))},
+                modifier = Modifier
+                    .padding(top = 20.dp)
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it},
+                label = {Text(stringResource(R.string.password))},
+                modifier = Modifier
+                    .padding(top = 20.dp)
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 15.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.iniciar_sesi_n)
+                )
+            }
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun loginP(){
+    login();
 }
